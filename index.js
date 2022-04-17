44c40f609f2c274e08a7d50d16f1768c3d61f1c6
shanecranor <70922464+shanecranor@users.noreply.github.com>
2022-04-17 12:45:41 -0600

    got multiple volumes working

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
+function setAttributes(element, attributes) {
+	Object.keys(attributes).forEach(attr => {
+	  element.setAttribute(attr, attributes[attr]);
+	});
+}
+function createElement(tag, attributes) {
+	let elem = document.createElement(tag)
+	setAttributes(elem, attributes)
+	return elem
+}
+
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 let masterLevel = audioCtx.createGain();
 masterLevel.connect(audioCtx.destination)
 oscs = []
-createOsc('square', 33.7,	.5)
-createOsc('square', 33.7*4,	.5)
+createOsc('square', 33.7,	.2)
+createOsc('square', 33.7*4,	.2)
 function createOsc(oscType, freq, gain){
-	oscs.push([audioCtx.createOscillator(), audioCtx.createGain()])
+	let volNode = audioCtx.createGain()
+	oscs.push([audioCtx.createOscillator(), volNode])
 	oscs[oscs.length-1][0].type = oscType
 	oscs[oscs.length-1][0].frequency.value = freq
-	oscs[oscs.length-1][0].connect(oscs[0][1])
+	oscs[oscs.length-1][0].connect(oscs[oscs.length-1][1])
 	oscs[oscs.length-1][1].gain.value = gain
 	oscs[oscs.length-1][1].connect(masterLevel)
 	oscs[oscs.length-1][0].start()
-	oscVolSlider = document.createElement('input')
-	oscVolSlider.id = 'osc' + (oscs.length-1) + 'Slider'
-	console.log('osc' + (oscs.length-1) + 'Slider' )
-	oscVolSlider.type = 'range'
-	oscVolSlider.min = 0
-	oscVolSlider.max = 100
-	oscVolSlider.value = 50
-	document.getElementById('oscControls').appendChild(oscVolSlider)
-	oscVolSlider.addEventListener('input', () => changeOscAmp(oscVolSlider))
+	let oscContainer = createElement('div', {
+		id: 'osc' + (oscs.length-1) + 'Container'
+	})
+	let oscAmp = createElement('input', {
+		class: 'amplitude',
+ 		type: 'range',
+		min: 0, max: 100, value: 50
+	})
+	let oscTypeDropdown = createElement('input', {
+		id: 'osc' + (oscs.length-1) + 'Slider',
+ 		type: 'range',
+		min: 0, max: 100, value: 50
+	})
+	oscContainer.appendChild(oscAmp)
+	//oscContainer.appendChild(oscTypeDropdown)
+	document.getElementById('oscControls').appendChild(oscContainer)
+	oscAmp.addEventListener('input', () => changeOscAmp(oscContainer))
 }
 
 const changeVolume = function(slider) {
@@ -28,19 +48,15 @@ const changeVolume = function(slider) {
 	masterLevel.gain.value = value
 }
 
-const changeOscAmp = function(slider) {
-	const value = slider.value / 100
-	oscIndex = slider.id[3]
-	console.log(slider.id)
-	oscs[oscIndex][1].gain.value = value
-	//console.log(oscs[oscIndex][1])
+const changeOscAmp = function(container) {
+	slider = container.querySelector('.amplitude')
+	oscIndex = container.id[3]
+	oscs[oscIndex][1].gain.value =  slider.value / 100
 }
+
 const changePitch = function(freq) {
 	oscs[0].frequency.value = freq
 }
 const masterVol  = document.querySelector('#masterVol')
-//const amplitude = document.querySelector('#amplitude')
-//const pitch = document.querySelector('#pitch')
-
 masterVol.addEventListener('input', () => changeVolume(masterVol))
-pitch.addEventListener('input', () => changePitch(pitch.value))
+// pitch.addEventListener('input', () => changePitch(pitch.value))
