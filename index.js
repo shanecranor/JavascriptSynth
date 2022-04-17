41416a8c01436cc76f7fd871598a98b746872bf4
shanecranor <70922464+shanecranor@users.noreply.github.com>
2022-04-17 13:02:39 -0600

    added multiple waveforms

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ function createElement(tag, attributes) {
 	return elem
 }
 
+let waveforms = {
+	0: 'sine',
+	1: 'triangle',
+	2: 'sawtooth',
+	3: 'square'
+}
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 let masterLevel = audioCtx.createGain();
 masterLevel.connect(audioCtx.destination)
-oscs = []
-createOsc('square', 33.7,	.2)
-createOsc('square', 33.7*4,	.2)
-function createOsc(oscType, freq, gain){
+let oscs = []
+createOsc(waveforms[2], 33.7,	.2)
+createOsc(waveforms[0], 33.7*4,	.2)
+console.log(waveforms)
+function createOsc(wave, freq, gain){
 	let volNode = audioCtx.createGain()
 	oscs.push([audioCtx.createOscillator(), volNode])
-	oscs[oscs.length-1][0].type = oscType
+	oscs[oscs.length-1][0].type = wave
 	oscs[oscs.length-1][0].frequency.value = freq
 	oscs[oscs.length-1][0].connect(oscs[oscs.length-1][1])
 	oscs[oscs.length-1][1].gain.value = gain
@@ -32,15 +39,17 @@ function createOsc(oscType, freq, gain){
  		type: 'range',
 		min: 0, max: 100, value: 50
 	})
-	let oscTypeDropdown = createElement('input', {
-		id: 'osc' + (oscs.length-1) + 'Slider',
+	let oscType = createElement('input', {
+		class: 'type',
  		type: 'range',
-		min: 0, max: 100, value: 50
+		min: 0, max: 3, value: 50
 	})
 	oscContainer.appendChild(oscAmp)
-	//oscContainer.appendChild(oscTypeDropdown)
-	document.getElementById('oscControls').appendChild(oscContainer)
 	oscAmp.addEventListener('input', () => changeOscAmp(oscContainer))
+	oscContainer.appendChild(oscType)
+	oscType.addEventListener('input', () => changeOscType(oscContainer))
+	document.getElementById('oscControls').appendChild(oscContainer)
+	
 }
 
 const changeVolume = function(slider) {
@@ -53,6 +62,13 @@ const changeOscAmp = function(container) {
 	oscIndex = container.id[3]
 	oscs[oscIndex][1].gain.value =  slider.value / 100
 }
+
+const changeOscType = function(container) {
+	slider = container.querySelector('.type')
+	oscIndex = container.id[3]
+	oscs[oscIndex][0].type =  waveforms[slider.value]
+}
+
 
 const changePitch = function(freq) {
 	oscs[0].frequency.value = freq
