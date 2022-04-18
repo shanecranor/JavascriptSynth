3679c385ed645b45688df9b6d84dd7291c9d35c9
shanecranor <70922464+shanecranor@users.noreply.github.com>
2022-04-17 18:21:16 -0600

    added keyboard support and button to create more OSC's

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+keyMap = new Object()
+
+document.onkeydown = function (e) {
+	if(!keyMap[e.key] || !keyMap[e.key].pressed){
+    	keyMap[e.key] = {pressed: true, time: performance.now()};
+		key = keyNoteMap[e.key]-9-24
+		note = eval(noteEquation.value)
+		for ( i in oscs ){
+			let newPitch = eval(oscs[i][2])
+			oscs[i][0].frequency.value = newPitch
+			oscs[i][0].connect(oscs[i][1])
+		}
+	}
+}
+
+document.onkeyup = function (e) {
+    keyMap[e.key] = {pressed: false, time: performance.now()};
+	let keysDown = 0
+	for ( key in keyMap ){
+		if(keyMap[key].pressed){
+			keysDown+=1
+		}
+	}
+	if (keysDown == 0){
+		for ( i in oscs ){
+			oscs[i][0].disconnect()
+		}
+	}
+}
+
+keyNoteMap = {
+	'q':	-1,
+	'a':	0,
+	'w':	1,
+	's':	2,
+	'e':	3,
+	'd':	4,
+	'f':	5,
+	't':	6,
+	'g' :	7,
+	'y' :	8,
+	'h' :	9,
+	'u' :	10,
+	'j' :	11,
+	'k' :	12,
+	'o' :	13,
+	'l' :	14,
+	'p' :	15,
+	';' :	16,
+	"'" :	17,
+	']' :	18,
+	'Enter':19
+}
+
+//https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
 function setAttributes(element, attributes) {
 	Object.keys(attributes).forEach(attr => {
 	  element.setAttribute(attr, attributes[attr]);
@@ -15,24 +70,32 @@ let waveforms = {
 	2: 'sawtooth',
 	3: 'square'
 }
+noteEquation = document.getElementById('noteEquation')
+noteEquation.value = '440*pow(pow(2, 1/12), key)'
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 let masterLevel = audioCtx.createGain();
 masterLevel.connect(audioCtx.destination)
 let oscs = []
-createOsc(waveforms[2], 33.7,	.2)
-createOsc(waveforms[0], 33.7*4,	.2)
+let key = 5
+let note = eval(noteEquation.value)
+createOsc(waveforms[2], 'note',	.2)
+createOsc(waveforms[0], 'note*4',	.2)
 console.log(waveforms)
-function createOsc(wave, freq, gain){
+function createOsc(wave, freqEquation, gain){
 	let volNode = audioCtx.createGain()
-	oscs.push([audioCtx.createOscillator(), volNode])
+	oscs.push([audioCtx.createOscillator(), volNode, freqEquation])
 	oscs[oscs.length-1][0].type = wave
-	oscs[oscs.length-1][0].frequency.value = freq
-	oscs[oscs.length-1][0].connect(oscs[oscs.length-1][1])
+	oscs[oscs.length-1][0].frequency.value = eval(freqEquation)
+	//oscs[oscs.length-1][0].connect(oscs[oscs.length-1][1])
 	oscs[oscs.length-1][1].gain.value = gain
 	oscs[oscs.length-1][1].connect(masterLevel)
 	oscs[oscs.length-1][0].start()
 	let oscContainer = createElement('div', {
 		id: 'osc' + (oscs.length-1) + 'Container'
+	})
+	let oscLabel = createElement('p', {
+		class: 'oscLabel',
+		style: 'display: inline; padding-right: 10px;'
 	})
 	let oscAmp = createElement('input', {
 		class: 'amplitude',
@@ -44,12 +107,29 @@ function createOsc(wave, freq, gain){
  		type: 'range',
 		min: 0, max: 3, value: 50
 	})
+	let oscPitch = createElement('input', {
+		class: 'pitch',
+ 		type: 'text',
+		value: freqEquation
+	})
+	let oscPitchVal = createElement('p', {
+		class: 'pitchVal',
+		style: 'display: inline; padding-left: 10px;'
+	})
+	oscLabel.innerText = 'osc' + (oscs.length-1)
+	oscContainer.appendChild(oscLabel)
 	oscContainer.appendChild(oscAmp)
 	oscAmp.addEventListener('input', () => changeOscAmp(oscContainer))
+	
 	oscContainer.appendChild(oscType)
 	oscType.addEventListener('input', () => changeOscType(oscContainer))
-	document.getElementById('oscControls').appendChild(oscContainer)
 	
+	oscContainer.appendChild(oscPitch)
+	oscPitch.addEventListener('input', () => changeOscPitch(oscContainer))
+
+	oscPitchVal.innerText = oscs[oscs.length-1][0].frequency.value
+	oscContainer.appendChild(oscPitchVal)
+	document.getElementById('oscControls').appendChild(oscContainer)
 }
 
 const changeVolume = function(slider) {
@@ -69,10 +149,57 @@ const changeOscType = function(container) {
 	oscs[oscIndex][0].type =  waveforms[slider.value]
 }
 
-
-const changePitch = function(freq) {
-	oscs[0].frequency.value = freq
+const changeOscPitch = function(container) {
+	input = container.querySelector('.pitch')
+	oscIndex = container.id[3]
+	oscs[oscIndex][2] = input.value
+	//oscs[oscIndex][0].frequency.value = freq
 }
 const masterVol  = document.querySelector('#masterVol')
 masterVol.addEventListener('input', () => changeVolume(masterVol))
 // pitch.addEventListener('input', () => changePitch(pitch.value))
+
+//https://www.sitepoint.com/delay-sleep-pause-wait/
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+t = 0
+function sin(x){
+	return Math.sin(x)
+}
+function cos(x){
+	return Math.cos(x)
+}
+function rand(){
+	return Math.random()
+}
+function pow(a,b){
+	return Math.pow(a,b)
+}
+
+async function updateLoop() {
+	t += 1
+
+	// for (i in keyMap){
+	// 	if(keyMap[i].pressed){
+	// 		key = keyNoteMap[i]-9-24
+	// 	}
+	// }
+	// note = eval(noteEquation.value)
+	let pitches = []
+	for ( i in oscs ){
+		pitches.push(oscs[i][0].frequency.value)
+	}
+	for ( i in oscs ){
+		try {
+			let newPitch = eval(oscs[i][2])
+			oscs[i][0].frequency.value = newPitch
+			if(t%5 == 0)
+				document.getElementById('osc'+i+'Container').querySelector('.pitchVal').innerText = newPitch
+		} catch (e) {}
+	}
+	await sleep(10)
+	updateLoop()
+}
+
+updateLoop()
