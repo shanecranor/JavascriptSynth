909de03c6f219612c64f8837a7a815b7a1537397
shanecranor <70922464+shanecranor@users.noreply.github.com>
2022-05-01 17:48:49 -0600

    fixed some minor bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.onkeydown = function (e) {
 	if(keyNoteMap[e.key] == null)
 		return
 	if(!keyMap[e.key] || !keyMap[e.key].pressed){
-    	keyMap[e.key] = {pressed: true, time: performance.now()};
+    	keyMap[e.key] = {pressed: true, time: performance.now()}
 		key = keyNoteMap[e.key]-9-24
 		note = eval(noteEquation.value)
 		for ( i in oscs ){
@@ -23,7 +23,7 @@ document.onkeydown = function (e) {
 document.onkeyup = function (e) {
 	if(keyNoteMap[e.key] == null)
 		return
-    keyMap[e.key] = {pressed: false, time: performance.now()};
+    keyMap[e.key] = {pressed: false, time: performance.now()}
 	let keysDown = 0
 	for ( key in keyMap ){
 		if(keyMap[key].pressed){
@@ -64,8 +64,8 @@ keyNoteMap = {
 //https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
 function setAttributes(element, attributes) {
 	Object.keys(attributes).forEach(attr => {
-	  element.setAttribute(attr, attributes[attr]);
-	});
+	  element.setAttribute(attr, attributes[attr])
+	})
 }
 function createElement(tag, attributes) {
 	let elem = document.createElement(tag)
@@ -87,26 +87,28 @@ function generateFunction(string) {
 noteEquation = document.getElementById('noteEquation')
 noteEquation.value = '440*pow(pow(2, 1/12), key)'
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-let masterLevel = audioCtx.createGain();
+let masterLevel = audioCtx.createGain()
 masterLevel.connect(audioCtx.destination)
 let oscs = []
 let key = 5
 let note = eval(noteEquation.value)
-createOsc(waveforms[3], 'note*4',	.2)
-createOsc(waveforms[3], 'note*floor(tan(t*100))',	.2)
-createOsc(waveforms[3], 'note*4*(floor(t*10)%5)',	0.0)
+createOsc(waveforms[3], 'note*2',	.2)
+createOsc(waveforms[3], 'note*4+sin(50*t)*4',	.2)
+createOsc(waveforms[3], 'note*(1+floor(t*8)%2)',	0.0)
 console.log(waveforms)
-function createOsc(wave, freqEquation, gain){
+function createOsc(wave, freqEquation, gain, volEquation){
 	let volNode = audioCtx.createGain()
 	let oscNode = {
 		osc:			audioCtx.createOscillator(), 
 		vol:			volNode,
 		freqText:		freqEquation, 
-		freqFunction:	generateFunction(freqEquation)
+		freqFunction:	generateFunction(freqEquation),
+		volEquation: 	gain,
+		volFunction:	generateFunction(volEquation)
 	}
 	oscs.push(oscNode)
 	oscs[oscs.length-1].osc.type = wave
-	oscs[oscs.length-1].osc.frequency.value = eval(freqEquation)
+	oscs[oscs.length-1].osc.frequency.value = oscs[oscs.length-1].freqFunction(note)
 	//oscs[oscs.length-1].osc.connect(oscs[oscs.length-1].vol)
 	oscs[oscs.length-1].vol.gain.value = gain
 	oscs[oscs.length-1].vol.connect(masterLevel)
@@ -121,6 +123,11 @@ function createOsc(wave, freqEquation, gain){
 		class: 'amplitude',
  		type: 'range',
 		min: 0, max: 100, value: gain*100
+	})
+	let oscAmpVal = createElement('input', {
+		class: 'pitch',
+ 		type: 'text',
+		value: volEquation
 	})
 	let oscType = createElement('input', {
 		class: 'type',
@@ -170,10 +177,12 @@ const changeOscType = function(container) {
 }
 
 const changeOscPitch = function(container) {
+	try {
 	input = container.querySelector('.pitch')
 	oscIndex = container.id[3]
 	oscs[oscIndex].freqText = input.value
 	oscs[oscIndex].freqFunction = generateFunction(oscs[oscIndex].freqText)
+	} catch (e){}
 	//oscs[oscIndex].osc.frequency.value = freq
 }
 const masterVol  = document.querySelector('#masterVol')
@@ -182,8 +191,35 @@ masterVol.addEventListener('input', () => changeVolume(masterVol))
 
 //https://www.sitepoint.com/delay-sleep-pause-wait/
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms))
 }
+// funky timer debugging shit note*1*(t*97.5-(f*1.5))
+let pitches = []
+let f = 0
+async function updateLoop() {
+	t = audioCtx.currentTime
+	f++
+	pitches = []
+	for ( i in oscs ){
+		pitches.push(oscs[i].osc.frequency.value)
+	}
+	for ( i in oscs ){
+		try {
+			let newPitch = oscs[i].freqFunction(note)
+			oscs[i].osc.frequency.setValueAtTime(newPitch, t)
+			
+			if(f%5 == 0)
+				document.getElementById('osc'+i+'Container').querySelector('.pitchVal').innerText = newPitch
+		} catch (e) {}
+	}
+	await sleep(10)
+	updateLoop()
+}
+
+updateLoop()
+
+//functions for use inside  the equations
+
 function sin(x){
 	return Math.sin(x)
 }
@@ -214,33 +250,3 @@ function clamp(a,b,c){
 	low = min(b,c)
 	return max(min(a,high),low)
 }
-
-let pitches = []
-let f = 0
-async function updateLoop() {
-	t = audioCtx.currentTime
-	f++
-	// for (i in keyMap){
-	// 	if(keyMap[i].pressed){
-	// 		key = keyNoteMap[i]-9-24
-	// 	}
-	// }
-	// note = eval(noteEquation.value)
-	pitches = []
-	for ( i in oscs ){
-		pitches.push(oscs[i].osc.frequency.value)
-	}
-	for ( i in oscs ){
-		try {
-			let newPitch = oscs[i].freqFunction(note)
-			oscs[i].osc.frequency.setValueAtTime(newPitch, audioCtx.currentTime)
-			
-			if(f%5 == 0)
-				document.getElementById('osc'+i+'Container').querySelector('.pitchVal').innerText = newPitch
-		} catch (e) {}
-	}
-	await sleep(10)
-	updateLoop()
-}
-
-updateLoop()
